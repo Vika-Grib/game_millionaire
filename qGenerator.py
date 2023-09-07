99d7525e6da30de40ae0a6471e12e5b3321e76bb
Vika <victoria.grib@gmail.com>
2023-09-07 19:03:38 +0300

    all files added for game + added comments

--- qGenerator.py
+++ qGenerator.py
@@ -4,12 +4,12 @@ from game import Game
 """автоматически генерирует тестовые файлы с вопросами"""
 LEVELS = Game._levels
 
-PROJECT_PATH = os.path.dirname(__file__)
+PROJECT_PATH = os.path.dirname(__file__) # __file_ = C:\Users\Lenovo\PycharmProjects\millionaire
 QUESTIONS_PATH = os.path.join(PROJECT_PATH, "test_questions")
 
 
 def generateQuestion(level, num):
-    correct_answ = num%4 if num%4 != 0 else 4
+    correct_answ = num%4 if num%4 != 0 else 4 #Если numне делится на 4, за правильный ответ принимается остаток; в противном случае правильный ответ устанавливается равным 4. Это способ гарантировать, что правильный номер ответа находится в диапазоне от 1 до 4 включительно.
     #print(f"{level} - question {num}?\tansw1\tansw2\tansw3\tansw4\t{correct_answ}\n")
     return f"{level} - question {num}?\tansw1\tansw2\tansw3\tansw4\t{correct_answ}\n"
 
@@ -25,5 +25,5 @@ def generateQuestionFiles():
             file.writelines(q)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # Этот условный оператор проверяет, запускается ли сценарий как основная программа (а не импортируется ли как модуль)
     generateQuestionFiles()
--- questions.py
+++ questions.py
@@ -11,26 +11,28 @@ from random import choice
 
 
 PROJECT_PATH = os.path.dirname(__file__)
-QUESTIONS_FOLDER = 'test_questions'
+#print(PROJECT_PATH)
+QUESTIONS_FOLDER = 'test_questions' # пут для смены вопросов на настоящие
 SEP = f"{'-'*50}"
 
 
 class Question:
     """
-    A separate question.
+    A separate question. / Для конкретного одного вопроса
     """
     def __init__(self, question, answ1, answ2, answ3, answ4, correctAnsw):
         self._question = question
         self._answers = [answ1, answ2, answ3, answ4]
-        self._curAnswers = self._answers
+        self._curAnswers = self._answers  # список вариантов вопросов на текущий момент - если использовал подсказки и тд
         self._correctAnswNum = correctAnsw
 
-    def __str__(self):
+    def __str__(self): # Метод __str__возвращает форматированное строковое представление вопроса
+                       # и вызывается когда выполняем команду print и передаем в неё обхект типа Question
         return f"{SEP}\n{self._question}\n{SEP}\n{self.getCurAnswersStr()}{SEP}"
 
     def isCorrectAnswer(self, answNum):
         """
-        Check if the answer is correct
+        Check if the answer is correct / проверяем правильность варианта ответа
         :param answNum: positional number of the answer
         :return: True/False
         """
@@ -38,14 +40,15 @@ class Question:
 
     def getCorrectAnswer(self):
         """
-        Get correct answer
+        Get correct answer / возвращаем правильный ответ
         :return: correct answer
         """
         return self._answers[self._correctAnswNum-1]
 
     def useHint(self, hint):
         """
-        Get a hint
+        Get a hint / для каждого вопроса можем использовать подсказку
+        - этот метод реализует использование подсказки
         :param hint: hint object
         :return: hint phrase
         """
@@ -58,6 +61,7 @@ class Question:
     def getCurAnswers(self):
         """
         Get current answers (could be only 2 answers after 50/50 hint)
+        Возвращает текущий вариант ответа: либо все 4, либо после подсказок
         :return: current answers
         """
         return self._curAnswers
@@ -65,6 +69,7 @@ class Question:
     def getCurAnswersStr(self):
         """
         Get current answers as string
+        Генерирует строчкуу для вывода вариантов ответа
         :return: string of current answers
         """
         return f"1: {self._curAnswers[0]:15} 3: {self._curAnswers[2]:15}\n" \
@@ -73,36 +78,43 @@ class Question:
 
 class Questions:
     """
-    Random questions from txt files
+    Random questions from txt files / Для списка вопросов
     """
     _folder = QUESTIONS_FOLDER
     _path = os.path.join(PROJECT_PATH, _folder)
 
     def __init__(self):
-        self._questions = None
-        self._loadQuestions()
+        self._questions = None  # список вопросов, которое изначально пустое
+                                # - в итоге тут мы получаем список элементов типа вот этого класса class Question:
+                                #     """
+                                #     A separate question. / Для конкретного одного вопроса
+                                #     """
+                                #     def __init__(self, question, answ1, answ2, answ3, answ4, correctAnsw):
+        self._loadQuestions()  # дальше загружаем вопросы
 
     def _loadQuestions(self):
         """
         Load random questions from txt files
         :return: None
         """
-        files = self._findQuestionFiles()
-        self._questions = [None]*len(files)
-        for file in files:
+        files = self._findQuestionFiles() # находим вопросы
+        self._questions = [None]*len(files) #список вопросов, длина кот равна количетсву  Строка кода инициализирует этот список Noneзначениями, а количество Noneзначений определяется длиной списка files.  Используя [None]*len(files), создаем список длиной, равной количеству файлов вопросов ( len(files)). Это гарантирует, что в списке будет место для каждого вопроса, который планируется загрузить.
+        for file in files: #  и далее поочередно начинамем читать и загружать фуже конкретные вопросы для конкретного уровня
             self._loadLevelQuestion(file)
 
     def _findQuestionFiles(self):
         """
-        Find txt files in the questions folder
+        Find txt files in the questions folder / Тут мы находим все текстовые файлы, которые находятся в обозначенной директории
+        Короче говоря, этот метод ищет в папке вопросов текстовые файлы, собирает их полные пути и возвращает их в списке
         :return: a list of files paths
         """
-        fileNames = os.listdir(Questions._path)
-        filePaths = []
+        fileNames = os.listdir(Questions._path) # Эта строка использует os.listdirфункцию для получения списка всех имен файлов и каталогов в каталоге, указанном Questions._path. Questions._path— это полный путь к папке с вопросами, как определено ранее в классе.
+        filePaths = []  # пустой список, который будет использоваться для хранения путей к текстовым файлам, найденным в папке
 
         for fileName in fileNames:
             filePath = os.path.join(Questions._path, fileName)
-            if os.path.isfile(filePath) and os.path.splitext(filePath)[1] == '.txt':
+            if os.path.isfile(filePath) and os.path.splitext(filePath)[1] == '.txt': # os.path.isfile(filePath)проверяет, действительно ли текущий элемент является файлом (а не каталогом)
+                                                # os.path.splitext(filePath)[1] == '.txt'проверяет, является ли расширение файла «.txt». os.path.splitextразбивает путь к файлу на кортеж, содержащий базовое имя и расширение, и [1]выбирает часть расширения.
                 filePaths.append(filePath)
 
         return filePaths
@@ -110,6 +122,10 @@ class Questions:
     def _loadLevelQuestion(self, filePath):
         """
         Load random question from specified txt file
+        эти строки кода считывают строку из файла, разбивают ее на компоненты
+        (вопрос, варианты ответа и правильный ответ), извлекают уровень или индекс из имени файла и
+        создают объект с извлеченной информацией Question. Этот процесс повторяется для каждой
+        строки файла, эффективно загружая несколько вопросов из файла и сохраняя их в _questionsсписке.
         :param filePath: path to txt file with questions
         :return: None
         """
@@ -118,12 +134,13 @@ class Questions:
 
         line = choice(data)
         q, a1, a2, a3, a4, ca = line[:-1].split("\t")   # without the last '/n'
-        ind = int(os.path.basename(filePath)[:-4])      # without the last '.txt'
-        self._questions[ind-1] = Question(q, a1, a2, a3, a4, int(ca))
+        ind = int(os.path.basename(filePath)[:-4])      # without the last '.txt' = «3.txt», на этом шаге цифра «3» извлекается как целое число.
+        self._questions[ind-1] = Question(q, a1, a2, a3, a4, int(ca)) # генерируем объект Question!
 
     def getQuestions(self):
         """
         Get a list of questions
+        Вернуть все случайно рандомно загруженные вопросы для данной игры
         :return: a list of questions
         """
         return self._questions
